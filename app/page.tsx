38f99acbf97a4165783df8a2dcdf32da39304e85
Koniczyn <koniczynszef@gmail.com>
2024-10-17 22:37:00 +0200

    chore: still trying to deploy - went back

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import { Features } from "@/components/features/Features";
 import { Hero } from "@/components/hero/Hero";
 import { Pricing } from "@/components/pricing/Pricing";
-// import { currentUser } from "@clerk/nextjs/server";
-// import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = object & {};
 
 const HomePage: React.FC<Props> = async () => {
-    // const user = await currentUser();
+    const user = await currentUser();
 
-    // if (user && !user?.username) {
-    //     redirect("/settings");
-    // }
+    if (user && !user?.username) {
+        redirect("/settings");
+    }
 
     return (
         <>
